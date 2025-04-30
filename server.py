eeebe5696c706c3dbc0ab7523471e51dabb2bfa3
SlavRoach <109216988+SlavRoach@users.noreply.github.com>
2025-04-30 09:53:44 +0200

    server.py

--- server.py
+++ server.py
@@ -45,4 +45,5 @@ def search():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))  # Na Renderi bude tento port automaticky priradený
+    app.run(host="0.0.0.0", port=port, debug=True)
